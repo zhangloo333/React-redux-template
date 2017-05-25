0380d7e9461a8d3ae39a2ac82ef9e99bd5d3ddc7
abc <zhangloo333@gmail.com>
2017-05-25 09:36:29 -0700

    frist update

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './public/src/index.js',
+    output: {
+        path: './public/src',
+        filename: './bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            },
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/
+        }]
+    }
+    // devServer: {
+    //     historyApiFallback: true,
+    //     contentBase: './'
+    // }
+};
